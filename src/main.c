ce314d17e0e6ee8af90521eb0bc136ef368f21e6
Shahidh K Muhammed <muhammedshahid.k@gmail.com>
2017-05-11 08:32:22 +0530

    [codeaway-console] Updates src/main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
 
 int main(){
-    printf("Hello World");
+    printf("Hello World\n");
     int a = 10;
     int b = 35;
-    printf("a + b = %d", a+b);
+    printf("a + b = %d\n", a+b);
+    printf("a * b = %d\n", a*b);
     return 0;
 }
